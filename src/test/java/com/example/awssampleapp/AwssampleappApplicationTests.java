90f16d8a949013f455304825feac27aa5dce760b
deveki-gmail <devekinandan@gmail.com>
2022-06-29 17:03:29 +0530

    initial version

---
+++ src/test/java/com/example/awssampleapp/AwssampleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awssampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwssampleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
